1e7894a3b09c86fb0d72d2368118f1c02b21cde0
moslem009 <sikdermoslem400@gmail.com>
2023-10-28 11:44:50 +0600

    Test Mesage

---
+++ test1.cpp
@@ -0,0 +1,22 @@
+//largest between two numbers   and returns the smallest    
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>       
+
+int main()
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+    printf("The smallest between %d and %d is %d\n",a,b,a+b);
+    return 0;
+}
---
+++ test2.py
@@ -0,0 +1,15 @@
+# find the largest between two numbers
+# three integer value 
+# q: what is even numbers   
+
+
+
+
+
+
+def get_even_numbers():
+
+    return 0
+
+
+
